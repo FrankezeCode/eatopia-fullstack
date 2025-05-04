dd4706b7e15b68345f996e779af7f0a365240968
frankezecode <Franklynezeugonna@gmail.com>
2025-05-04 18:47:44 +0100

    updated product category page

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -14,14 +14,14 @@ const ProductCategory = () => {
   return (
     <div className='mt-16'>
       {searchCategory && (
-        <div className='flex flex-col items-end w-max'>
+        <div className='flex flex-col items-end max-md:items-center md:w-max'>
           <p className='text-2xl font-medium'>{searchCategory.text.toUpperCase()}</p>
           <div className='w-16 h-0.5 bg-primary rounded-full'></div>
         </div>
       )}
 
       {filteredProducts.length > 0 ? (
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6  mt-6'>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6  mt-6 max-md:grid-cols-1 max-md:place-items-center'>
           {filteredProducts.map((product)=>(
               <ProductCard key={product._id}  product={product}/>
           ))}
